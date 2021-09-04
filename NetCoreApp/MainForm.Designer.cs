8c5cc27cd75afdd039ee617bb7a4296d50f1ae82
setsuodu <339131001@qq.com>
2021-09-04 13:07:31 +0800

    commit code

---
+++ NetCoreApp/MainForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace WinFormsApp1
+{
+    public class User
+    {
+        //[BsonId]
+        //public ObjectId Id { get; set; }
+        public string name { get; set; }
+        public int number { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string name, int number)
+        {
+            //this.Id = id;
+            this.name = name;
+            this.number = number;
+        }
+    }
+
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MongoDB";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.connectToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(118, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maxToolStripMenuItem,
+            this.windowToolStripMenuItem});
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.showToolStripMenuItem.Text = "显示";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.ShowToolStripMenuItem_Click);
+            // 
+            // maxToolStripMenuItem
+            // 
+            this.maxToolStripMenuItem.Name = "maxToolStripMenuItem";
+            this.maxToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.maxToolStripMenuItem.Text = "Max";
+            // 
+            // windowToolStripMenuItem
+            // 
+            this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.windowToolStripMenuItem.Text = "Window";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.exitToolStripMenuItem.Text = "退出";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.connectToolStripMenuItem.Text = "连接DB";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.ConnectToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 665);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Winform+NetCore3.1+MongoDB";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            //string connectionString = "mongodb://localhost";
+            string connectionString = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&ssl=false";
+            //string connectionString = "mongodb+srv://<username>:<password>@<cluster-address>/test?w=majority";
+            Debug.Print($"connect to: {connectionString}");
+
+            var client = new MongoClient(connectionString);
+
+            //var search1 = client.ListDatabaseNames().ToList();
+            //Debug.Print($"有{search1.Count}个database");
+            //for (int i = 0; i < search1.Count; i++)
+            //{
+            //    Debug.Print($"[{i}]---{search1[i]}");
+            //}
+            //有4个database
+            //[0]---admin
+            //[1]---config
+            //[2]---local
+            //[3]---stickerDB
+
+            //  根据数据库名称实例化数据库
+            var database = client.GetDatabase("stickerDB");
+
+            // 根据集合名称获取集合
+            var collection = database.GetCollection<BsonDocument>("Users");
+            var filter = new BsonDocument();
+
+            // 查询集合中的文档
+            var search2 = Task.Run(async () => await collection.Find(filter).ToListAsync()).Result;
+            // 循环遍历输出
+            search2.ForEach(p =>
+            {
+                Debug.Print($"姓名：{p["name"]}，球衣号码：{p["number"]}");
+            });
+
+            //$lt    <   (less  than)
+            //$lte   <=  (less than  or equal to)
+            //$gt    >   (greater  than)
+            //$gte   >=  (greater  than or equal to)
+            //var filter3 = Builders<BsonDocument>.Filter.Eq("number", 10); //等于
+            var filter3 = Builders<BsonDocument>.Filter.Gte("number", 10); //大于等于
+            var result = collection.Find(filter3);
+            Debug.Print($"result={result.CountDocuments()}");
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem showToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem maxToolStripMenuItem;
+        private ToolStripMenuItem windowToolStripMenuItem;
+        private ToolStripMenuItem connectToolStripMenuItem;
+    }
+}
---
+++ NetCoreApp/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.SizeChanged += new EventHandler(Form1_SizeChanged);
+            this.notifyIcon1.MouseClick += new MouseEventHandler(notifyIcon1_MouseClick);
+            this.notifyIcon1.DoubleClick += new EventHandler(notifyIcon1_DoubleClick);
+
+            //Debug.Print("count---" + this.notifyIcon1.ContextMenuStrip.Items.Count);
+            //Debug.Print("0---" + this.notifyIcon1.ContextMenuStrip.Items[0].Name);
+            //Debug.Print("1---" + this.notifyIcon1.ContextMenuStrip.Items[1].Name);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case FormWindowState.Normal:
+                    Debug.Print("还原窗口");
+                    break;
+                case FormWindowState.Minimized:
+                    this.Hide(); //隐藏主窗体
+                    this.notifyIcon1.Visible = true; //让notifyIcon1图标显示
+                    Debug.Print("最小化");
+                    break;
+                case FormWindowState.Maximized:
+                    Debug.Print("最大化");
+                    break;
+            }
+        }
+
+        //当鼠标点击托管图标时触发
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) //当鼠标点击为左键时
+            {
+                Debug.Print("左键点击");
+
+                //this.Show(); //显示主窗体
+                //this.WindowState = FormWindowState.Normal; //主窗体的大小为默认
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Debug.Print("右键点击");
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Debug.Print("双击");
+
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+            else
+            {
+                /*
+                //还原窗体显示
+                this.WindowState = FormWindowState.Normal;
+                //激活窗体并给予它焦点
+                this.Activate();
+                //任务栏区显示图标
+                this.ShowInTaskbar = true;
+                //托盘区图标隐藏
+                this.notifyIcon1.Visible = false;
+                */
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void ConnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Debug.Print("连接MongoDB");
+        }
+
+        private void ShowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("是否确认退出程序？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // 关闭所有的线程
+                //this.Dispose();
+                //this.Close();
+                Debug.Print("关闭服务器");
+            }
+        }
+    }
+}
